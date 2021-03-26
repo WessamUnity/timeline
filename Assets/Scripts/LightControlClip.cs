b3c47bc9594faa0cc5c175c31947e49ad1e27654
Wessam <wessamfathi@gmail.com>
2021-03-26 11:17:09 +0100

    Sample clip controlled by custom behaviour

---
+++ Assets/Scripts/LightControlClip.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+[Serializable]
+public class LightControlClip : PlayableAsset, ITimelineClipAsset
+{
+    [SerializeField]
+    private LightControlBehaviour template = new LightControlBehaviour();
+
+    public ClipCaps clipCaps => ClipCaps.None;
+
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+    {
+        return ScriptPlayable<LightControlBehaviour>.Create(graph, template);
+    }
+}
