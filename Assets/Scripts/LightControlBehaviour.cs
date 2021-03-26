37d7530d4701dcdcd6d6b6b8cbae25779d1c5244
Wessam <wessamfathi@gmail.com>
2021-03-26 11:16:53 +0100

    Sample behaviour to control the timeline

---
+++ Assets/Scripts/LightControlBehaviour.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[Serializable]
+public class LightControlBehaviour : PlayableBehaviour
+{
+    [SerializeField]
+    private Color color = Color.white;
+
+    [SerializeField]
+    private float intensity = 1f;
+
+    [SerializeField]
+    private float bounceIntensity = 1f;
+
+    [SerializeField]
+    private float range = 10f;
+
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+        var light = playerData as Light;
+
+        if (light != null)
+        {
+            light.color = color;
+            light.intensity = intensity;
+            light.bounceIntensity = bounceIntensity;
+            light.range = range;
+        }
+    }
+}
